f0e792f88fb389c431617cfa882868cb1be077e2
irfan fadillah <irfanfdllh0@gmail.com>
2021-09-11 16:06:42 +0700

    Menambahkan OOP

---
+++ oop/latihan2.php
@@ -0,0 +1,36 @@
+<?php 
+{
+    // mendefinisikan sebuah class
+    class segitiga
+     {
+        //membuat property atau atribute
+        public $alas = 10;
+        public $tinggi = 15;
+        public $a = 10;
+        public $b =  15;
+        public $c = 10;
+        //membuat method atau behaviour
+        public function luassegitiga()
+        {
+            
+            return ($this-> alas * $this-> tinggi / 2);
+        }
+        public function kelilingsegitiga()
+        {
+            
+            return ($this->a + $this->b + $this->c );
+        }
+    }
+    //membuat object instalasi object 
+    $segitiga1 = new segitiga();
+    echo "Alas Segita : ".$segitiga1 -> alas. "<br>";
+    echo "Tinggi Segitiga : ".$segitiga1 -> tinggi. "<br>";
+    echo "Sisi 1 : ".$segitiga1 -> a  . "<br>";
+    echo "Sisi 2 : ".$segitiga1 -> b  . "<br>";
+    echo "Sisi 3 : ".$segitiga1 -> c  . "<br>";
+    echo "Luas Segitiga : ".$segitiga1 -> luassegitiga(). "<br>";
+    echo "Keliling Segitiga : ".$segitiga1 -> kelilingsegitiga(). "<br>";
+    echo "<hr>";
+    
+}
+?>
---
+++ oop/oop1.php
@@ -0,0 +1,26 @@
+<?php
+// mengedintefikasi sebuah kelas
+class Kucing{
+    public $warna ="Coklat";
+    public $jumlahKaki = "4";
+    public $jenisBulu = "panjang";
+    public $makananfavorit = "Ikan Pindang ";
+
+    public function bersuara(){
+        return "meowng....meowng.....meowng";
+    }
+    public function berburu(){
+        return "Berburu Ikan";
+    }
+    
+}
+// memebuat object (intisiasi object)
+$kucing1 = new Kucing();
+echo "warna kucing : ".$kucing1->warna . "<br>";
+echo "Sifat kucing : ".$kucing1->bersuara()."<br>";
+echo "<hr>";
+$kucing2 = new Kucing();
+//set warna kucing
+$kucing2->warna = "oren";
+echo "warna kucing : ".$kucing2->warna . "<br>";
+echo "Sifat kucing : ".$kucing2->berburu()."<br>";
